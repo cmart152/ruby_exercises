c3272a47d2dd3eaf78c555f56e46f432d95be30a
Chester Martin <chester.martin89@gmail.com>
2021-10-06 17:01:21 +0100

    bubble sort exercises added

---
+++ bubbleSort.rb
@@ -0,0 +1,22 @@
+def bubble_sort(array) 
+
+    flag = true
+    
+      while flag == true
+        flag = false
+        array.each_with_index do |element, index|
+          next_index = index+1
+          if array[next_index] == nil
+           
+          else
+            if array[index]>array[next_index]
+              array[index], array[next_index] = array[next_index],array[index]
+              flag = true
+            end
+          end
+        end
+      end
+    array
+    end
+    
+    p bubble_sort([4,3,78,2,0,2])
